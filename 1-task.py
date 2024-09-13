c8ad6768e68e2e85089c28116c718fc6ac9ce7e2
OybekBoltaboyev <oybekboltaboyev1443@gmail.com>
2024-09-13 21:13:23 +0500

    Add files via upload

---
+++ 1-task.py
@@ -0,0 +1,45 @@
+"1-misol"
+# son1=0
+# son2=0
+# summa=0
+#
+# son1=int(input("1-sonni kiriting: "))
+# son2=int(input("2-sonni kiriting: "))
+#
+# summa=son1+son2
+#
+# print(f"Javob: {summa}")
+
+
+"2-misol"
+#
+# a=0
+# b=0
+# c=0
+#
+# a=int(input("1-sonni kiriting: "))
+# b=int(input("2-sonni kiriting: "))
+# c=int(input("3-sonni kiriting: "))
+#
+# if a>c:
+#     if a>b:
+#         print(f"Eng katta son {a}")
+#     else:
+#         print(f"Eng katta son {b}")
+#
+# elif c>a:
+#     if c>b:
+#         print(f"Eng katta son {c}")
+#     else:
+#         print(f"Eng katta son {b}")
+
+"3-misol"
+#
+# def faktorial(n):
+#     i=1
+#     fakt=1
+#     while i!=n+1:
+#         fakt=fakt*i
+#         i+=1
+#     return fakt
+# print(faktorial(5))
